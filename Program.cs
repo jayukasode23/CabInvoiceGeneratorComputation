b179316a759f0b5dd02c2198928794f6a97274a6
jayukasode23 <jayukasode2398@gmail.com>
2022-11-12 18:57:21 +0530

    Added Cab Invoice Generator

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace CabInvoiceGeneratorProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------Welcome To Cab Invoice Generator Program------------");
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+
+            double fare = invoiceGenerator.CalculateFare(2.0, 5);
+            Console.WriteLine(fare);
+        }
+    }
+}
